cd71b05ef6e5ecdad36bada06b50e703284c5562
Raul Ferrandez <ferrandezraul@gmail.com>
2015-05-31 20:20:55 +0200

    foo

--- db/migrate/001_schema.rb
+++ db/migrate/001_schema.rb
@@ -2,6 +2,8 @@ class Schema < ActiveRecord::Migration
   def change
     create_table :products, force: true do |t|
       t.string :name
+      t.decimal :price, precision: 2, scale: 2
+      t.decimal :weight, precision: 2, scale: 2
     end
   end
 end
