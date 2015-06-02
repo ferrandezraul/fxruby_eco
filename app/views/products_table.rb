e300d9978b3d650004b07bc645e673e8ed9af3c8
Raul Ferrandez <raul.ferrandez@here.com>
2015-06-02 17:00:53 +0200

    foo

---
+++ app/views/products_table.rb
@@ -0,0 +1,33 @@
+include Fox
+
+class ProductsTable < FXTable
+  attr_reader :product
+  
+  def initialize(parent, products)
+    super(parent, :opts => LAYOUT_FILL)
+
+    @products = products
+
+    $APPLOG.debug "Number of products: #{@products.count}"
+
+    setTableSize(@products.count, 2)
+
+    setColumnText(0, "Name")
+    setColumnText(1, "Price")
+
+    rowHeaderMode = ~LAYOUT_FIX_WIDTH
+    columnHeader.setItemJustify(0, FXHeaderItem::CENTER_X)
+    columnHeader.setItemJustify(1, FXHeaderItem::CENTER_X)
+
+    @products.each_with_index do | product, index |
+      setItemText( index, 0, product.name )
+      setItemText( index, 1, product.price.to_s )
+    end
+  end
+
+  def create
+    super
+    show()
+  end
+
+end
--- app/views/products_view.rb
+++ app/views/products_view.rb
@@ -1,33 +1,16 @@
+require 'products_table'
+
 include Fox
 
-class ProductsView < FXTable
+class ProductsView < FXPacker
   attr_reader :product
   
   def initialize(parent, products)
     super(parent, :opts => LAYOUT_FILL)
 
-    @products = products
+    FXLabel.new(self, "Table of products:")
+    ProductsTable.new( self, products )
 
-    $APPLOG.debug "Number of products: #{@products.count}"
-
-    setTableSize(@products.count, 2)
-
-    setColumnText(0, "Name")
-    setColumnText(1, "Price")
-
-    rowHeaderMode = ~LAYOUT_FIX_WIDTH
-    columnHeader.setItemJustify(0, FXHeaderItem::CENTER_X)
-    columnHeader.setItemJustify(1, FXHeaderItem::CENTER_X)
-
-    @products.each_with_index do | product, index |
-      setItemText( index, 0, product.name )
-      setItemText( index, 1, product.price.to_s )
-    end
-  end
-
-  def create
-    super
-    show()
   end
 
 end
