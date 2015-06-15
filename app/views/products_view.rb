5a96d771d85f11e667be335a311f22bd4e6dbf84
Raul Ferrandez <ferrandezraul@gmail.com>
2015-06-15 22:59:25 +0200

    foo

--- app/views/products_view.rb
+++ app/views/products_view.rb
@@ -11,23 +11,24 @@ class ProductsView < FXPacker
 
     # button to add a new product
     button_new_product = FXButton.new( self, "Add new product", :opts => BUTTON_NORMAL)
-	button_new_product.connect(SEL_COMMAND) do |sender, sel, data| 
-
-		product_dialog = ProductDialog.new( self )
-		if product_dialog.execute != 0
-		    name = product_dialog.product[:name].value
-		    price = product_dialog.product[:price].value
-
-		    # Show dialog for entering data
-			FXMessageBox.warning( self, 
-								  MBOX_OK, 
-								  "Create new product", 
-								  "New product #{name} with a price #{price}" )
-		end
-	end
+	button_new_product.connect(SEL_COMMAND, method(:on_add_product) )
 
     ProductsTable.new( self, products )
-
   end
+
+  def on_add_product( sender, sel, data )
+  	product_dialog = ProductDialog.new( self )
+	if product_dialog.execute != 0
+	    name = product_dialog.product[:name].value
+	    price = product_dialog.product[:price].value
+
+	    # Show dialog for entering data
+		FXMessageBox.warning( self, 
+							  MBOX_OK, 
+							  "Create new product", 
+							  "New product #{name} with a price #{price}" )
+	end
+  end
+
 
 end
