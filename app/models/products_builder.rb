8a833a0c9c3b36ac5bb2444900dda3e5269da65b
Raul Ferrandez <ferrandezraul@gmail.com>
2015-06-29 15:04:35 +0200

    foo

--- app/models/products_builder.rb
+++ app/models/products_builder.rb
@@ -2,14 +2,29 @@ require 'csv'
 
 class ProductsBuilder
 
-	def self.from_csv( file )
+	def self.create_from_csv( file )
 		raise Exception.new("#{file} does npot exist!!") if !File.exist?(file)
+		
+		Product.destroy_all
+
 		# TODO (handle errors in csv)
 	    CSV.foreach(file, :headers => true) do |csv_row|
 	      #puts "Row is #{csv_row}" #puts "Row is #{csv_row.class}" #puts "Row inspect is #{csv_row.inspect}"
 	      #puts "Row inspect is #{csv_row.to_hash}"
 	      Product.create!( csv_row.to_hash )
 	    end
+	end
+
+	def self.build_from_csv( file )
+		raise Exception.new("#{file} does npot exist!!") if !File.exist?(file)
+		
+		products = Array.new
+		
+	    CSV.foreach(file, :headers => true) do |csv_row|
+	      products << Product.new( csv_row.to_hash )
+	    end
+
+	    products
 	end
 
 end
--- spec/products_builder_spec.rb
+++ spec/products_builder_spec.rb
@@ -8,10 +8,30 @@ describe ProductsBuilder do
 		# Do something before any single test
 	end
 
+	it "creates an array with products without creating them on database" do
+		products = ProductsBuilder::build_from_csv("db/products.csv")
+
+		expect(Product.count).to eq(0)
+		expect(products.count).to eq(4)
+	end
+
  	it "creates products in the database from a csv file" do
 	  	expect( Product.count ).to eq(0)
 
-	  	ProductsBuilder::from_csv( "db/products.csv" )
+	  	ProductsBuilder::create_from_csv( "db/products.csv" )
+
+	    expect( Product.count ).to eq(4)
+	end
+
+	it "deletes existing products in database when creating from csv" do
+		Product.create!( :name => "What ever", 
+						 :price_type => Product::PriceType::POR_KILO,
+						 :price => 2.5,
+						 :tax_percentage => 4 )
+
+	  	expect( Product.count ).to eq(5)
+
+	  	ProductsBuilder::create_from_csv( "db/products.csv" )
 
 	    expect( Product.count ).to eq(4)
 	end
