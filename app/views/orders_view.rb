c589096557967828456b0815a470f3b95323920d
Raul Ferrandez <raul.ferrandez@here.com>
2015-07-02 16:43:04 +0200

    foo

--- app/views/orders_view.rb
+++ app/views/orders_view.rb
@@ -14,18 +14,24 @@ class OrdersView < FXPacker
     button_new_order = FXButton.new( self, "Add new order", :opts => BUTTON_NORMAL)
 
 	button_new_order.connect(SEL_COMMAND) do |sender, sel, data|
-
-		date_dialog = DateDialog.new(self)
-		if date_dialog.execute != 0
-
-		  	order_dialog = OrderDialog.new( self, date_dialog.date )
-  			if order_dialog.execute != 0
-		        @table.reset
-		    end
-		end
+		get_new_order		
     end
 
     @table = OrdersTable.new( self )
+  end
+
+  def get_new_order
+  	order_dialog = OrderDialog.new( self, get_date )
+		if order_dialog.execute != 0
+        @table.reset
+    end
+  end
+
+  def get_date
+  	date_dialog = DateDialog.new(self)
+	if date_dialog.execute != 0
+	  	date_dialog.date
+	end
   end
 
   def reset
