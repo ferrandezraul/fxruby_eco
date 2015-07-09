4784048aa20ad8f20e25fd0056805de135215e67
Raul Ferrandez <ferrandezraul@gmail.com>
2015-07-10 00:02:54 +0200

    foo

--- app/views/line_item_dialog.rb
+++ app/views/line_item_dialog.rb
@@ -15,75 +15,79 @@ class LineItemDialog < FXDialogBox
 		add_terminating_buttons
 	end
 
-	def construct_page	    
-	    form = FXVerticalFrame.new( self, :opts => LAYOUT_FILL)
-	    matrix = FXMatrix.new( form, 2, :opts => MATRIX_BY_COLUMNS|LAYOUT_FILL_X )
-
-	    product_frame = FXHorizontalFrame.new( form, :opts => LAYOUT_FILL_X )
-	    price_frame = FXHorizontalFrame.new( form, :opts => LAYOUT_FILL_X )
+	def quantity_and_weight(form)
+		matrix = FXMatrix.new( form, 2, :opts => MATRIX_BY_COLUMNS|LAYOUT_FILL_X )
 	    
-	    FXLabel.new( matrix, "Quantity:" )
-	    quantity = FXTextField.new(matrix, 20, 
-	        :opts => TEXTFIELD_NORMAL|LAYOUT_FILL_X|LAYOUT_FILL_COLUMN)
-	    quantity.text = "Press any key and enter quantity ..."
-	    quantity.connect(SEL_KEYPRESS) do |sender, sel, data|
-	    	cantidad = FXInputDialog.getInteger(0, self, 
-	    		"Quantity", "Quantity", nil, 1, 1000)
-	    	@item[:quantity] = cantidad
-	    	sender.text = "#{cantidad.to_s}"
-	    end
+	  FXLabel.new( matrix, "Quantity:" )
+	  quantity = FXTextField.new(matrix, 20, 
+	      :opts => TEXTFIELD_NORMAL|LAYOUT_FILL_X|LAYOUT_FILL_COLUMN)
+	  quantity.text = "Press any key and enter quantity ..."
 
-	    FXLabel.new( matrix, "Weight:" )
-	    weight = FXTextField.new(matrix, 20, 
-	        :opts => TEXTFIELD_NORMAL|LAYOUT_FILL_X|LAYOUT_FILL_COLUMN)
-	    weight.text = "Press any key and enter weight in kg ..."
-	    weight.connect(SEL_KEYPRESS) do |sender, sel, data|
-	    	peso = FXInputDialog.getReal(0, self, 
-	    		"Weight", "Weight", nil, 0, 1000)
-	    	@item[:weight] = peso
-	    	sender.text = "#{peso.to_s} Kg"
-	    end
-	    weight.enabled = false
+	  quantity.connect(SEL_KEYPRESS) do |sender, sel, data|
+	  	cantidad = FXInputDialog.getInteger(0, self, 
+	    	"Quantity", "Quantity", nil, 1, 1000)
+	  	@item[:quantity] = cantidad
+	  	sender.text = "#{cantidad.to_s}"
+		end
 
-	    FXLabel.new( product_frame, "Product:" )
+	  FXLabel.new( matrix, "Weight:" )
+	  @weight_field = FXTextField.new(matrix, 20, 
+	      :opts => TEXTFIELD_NORMAL|LAYOUT_FILL_X|LAYOUT_FILL_COLUMN)
+	  @weight_field.text = "Press any key and enter weight in kg ..."
+	    
+	  @weight_field.connect(SEL_KEYPRESS) do |sender, sel, data|
+	  	peso = FXInputDialog.getReal(0, self, 
+	  		"Weight", "Weight", nil, 0, 1000)
+	  	@item[:weight] = peso
+	  	sender.text = "#{peso.to_s} Kg"
+	  end
+	  @weight_field.enabled = false
+	end
+
+
+	def construct_page
+		form = FXVerticalFrame.new( self, :opts => LAYOUT_FILL)
+
+		quantity_and_weight(form)
+
+		product_frame = FXHorizontalFrame.new( form, :opts => LAYOUT_FILL_X )
+	  price_frame = FXHorizontalFrame.new( form, :opts => LAYOUT_FILL_X )
+
+	  FXLabel.new( product_frame, "Product:" )
 		@product_combo_box = FXComboBox.new(product_frame, 
 			20, nil, 0, LAYOUT_FILL_Y, 20, 20 ) 
 
 		price_label = FXLabel.new( product_frame, "Price:", :opts => LAYOUT_FILL_X )
-	    price_label.justify = JUSTIFY_RIGHT
+	  price_label.justify = JUSTIFY_RIGHT
 
-	    iva_label = FXLabel.new( product_frame, "IVA:", :opts => LAYOUT_FILL_X )
-	    iva_label.justify = JUSTIFY_RIGHT
+	  iva_label = FXLabel.new( product_frame, "IVA:", :opts => LAYOUT_FILL_X )
+	  iva_label.justify = JUSTIFY_RIGHT
 
-	    total_label = FXLabel.new( product_frame, "Total:", :opts => LAYOUT_FILL_X )
-	    total_label.justify = JUSTIFY_RIGHT
+    total_label = FXLabel.new( product_frame, "Total:", :opts => LAYOUT_FILL_X )
+	  total_label.justify = JUSTIFY_RIGHT
 
-	    Product.all.where( :outdated => false ).each do | product |
+	  Product.all.where( :outdated => false ).each do | product |
 	    	@product_combo_box.appendItem( product.name, product )
-	    end
+	  end
 
-	    @product_combo_box.connect(SEL_COMMAND) do |sender, sel, text|
-	    	index = sender.findItem(text)
-	    	product = sender.getItemData( index )
+	  @product_combo_box.connect(SEL_COMMAND) do |sender, sel, text|
+	  	index = sender.findItem(text)
+	   	product = sender.getItemData( index )
 
-	    	raise Exception.new("product can not be nil") if product.nil? 
+	   	raise Exception.new("product can not be nil") if product.nil? 
+	   	@weight_field.enabled = ( product.price_type == Product::PriceType::POR_KILO )
 
-	    	if product.price_type == Product::PriceType::POR_KILO
-	    		weight.enabled = true # Enable text field weight
-	    	else
-	    		weight.enabled = false
-	    	end
-	    	price_label.text = "Price: #{product.price} EUR"
-	    	iva_label.text = "IVA: #{product.tax_percentage} %"
-	    	total_label.text = "Total: #{product.total} EUR"
-	    	@item[:product] = product
-	    end 
+	   	price_label.text = "Price: #{product.price} EUR"
+	   	iva_label.text = "IVA: #{product.tax_percentage} %"
+	   	total_label.text = "Total: #{product.total} EUR"
+	   	@item[:product] = product
+	  end 
 
-	    # This needs to be after the connection above
-	    # otherwise @item[:product] = nil although current product in combobox is not nil
-	    @product_combo_box.setCurrentItem(1, true) if Product.count > 0
-	    @product_combo_box.editable = false 
-	    #@product_combo_box.numVisible( 5 ) # not working
+	  # This needs to be after the connection above
+	  # otherwise @item[:product] = nil although current product in combobox is not nil
+	  @product_combo_box.setCurrentItem(1, true) if Product.count > 0
+	  @product_combo_box.editable = false 
+	  #@product_combo_box.numVisible( 5 ) # not working
 	end
 
 	def add_terminating_buttons
