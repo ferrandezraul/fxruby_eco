66524d4554967fedea7b5e96c1060e7d3c68a140
Raul Ferrandez <ferrandezraul@gmail.com>
2015-07-12 20:57:26 +0200

    foo

--- spec/models/line_items_spec.rb
+++ spec/models/line_items_spec.rb
@@ -4,28 +4,25 @@ describe LineItem do
   before do
     # Do something before any single test
     delete_all_records
+  end
 
-    # uses Factories defined in factories.rb
+  it "is deleted when the order is deleted without deleting the product" do
     @soca = create(:soca)
     @pigat = create( :product, :name => "Pigat", :price_type => Product::PriceType::POR_UNIDAD, :price => 5, :tax_percentage => 4 )
 
     @raul = create(:customer)
-    @carmen = create(:customer, :name => 'Carmen', :address => 'Riudaura', :nif => '23238768Y')
 
     @raul_order = Order.create!( :date => Time.now, :customer => @raul )
-    @carmen_order = Order.create!( :date => Time.now, :customer => @carmen )
-  end
 
-  it "is deleted when the order is deleted without deleting the product" do 
     @raul_order.line_items.create!( :quantity => 1, :weight => 0, :product => @soca )
 
-    expect( Order.count ).to eq(2)
+    expect( Order.count ).to eq(1)
     expect( LineItem.count ).to eq(1)
     expect( Product.count ).to eq(2)
 
     @raul_order.destroy # Calling delete would not work
 
-    expect( Order.count ).to eq(1)
+    expect( Order.count ).to eq(0)
     expect( LineItem.count ).to eq(0)
     expect( Product.count ).to eq(2)
   end 
